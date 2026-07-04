faf0ed1c0dad43082e93890b2075bac6a1f39b6a
Niyati Ahuja <niyati.ahuja2007>
2026-07-04 19:14:37 +0530

    Updates SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,7 @@ import { FaSearch } from "react-icons/fa";
 
 function SearchBar({ searchQuery, setSearchQuery }) {
   return (
-    <div className="flex items-center w-full max-w-md md:max-w-lg lg:max-w-xl h-12 px-4 gap-3 rounded-full bg-zinc-900 border border-zinc-700 focus-within:border-red-500 transition-all duration-300">
-      <FaSearch className="text-gray-400 text-lg" />
+    <div className="flex items-center w-full h-12 px-4 gap-3 rounded-full bg-zinc-900 border border-zinc-700 focus-within:border-red-500 transition-all duration-300">
 
       <input
         type="text"
